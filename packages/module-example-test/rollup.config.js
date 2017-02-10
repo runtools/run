4adcdf0f2d4e89ef9e4a3b6c51fb3e228b052b66
Manuel Vila <mvila@3base.com>
2017-02-11 08:18:19 +0900

    Simplify build by using rollup-plugin-babel

--- packages/module-example-test/rollup.config.js
+++ packages/module-example-test/rollup.config.js
@@ -1,14 +1,31 @@
+import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import globals from 'rollup-plugin-node-globals';
 
 export default {
-  entry: 'es5/index.js',
+  entry: 'src/index.js',
   dest: 'dist/index.js',
   format: 'iife',
   moduleName: 'bundle',
   plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      presets: [
+        [
+          'env',
+          {
+            targets: {
+              ie: 10,
+              node: 4
+            },
+            loose: true,
+            modules: false
+          }
+        ]
+      ]
+    }),
     nodeResolve({ browser: true }),
     commonjs(),
     json(),
