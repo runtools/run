6b9a8479e1ee769178c0fba89a4755710b50f4db
Manuel Vila <mvila@3base.com>
2017-02-16 09:09:05 +0900

    website: Continue initial implementation

--- packages/website/src/lib/deployer.js
+++ packages/website/src/lib/deployer.js
@@ -1,5 +1,22 @@
 'use strict';
 
+import { join, dirname, basename } from 'path';
+import { tmpdir } from 'os';
+import { remove } from 'fs-promise';
+import { generateDeploymentName } from '@voila/common';
+import { bundle } from './bundler';
+
 export async function deploy({ entryFile, name, version, stage, awsConfig }) {
-  // TODO
+  const deploymentName = generateDeploymentName({ name, version, stage });
+  const outputDir = join(
+    tmpdir(), 'voila-website', 'deployer', 'outputs', deploymentName
+  );
+  await remove(outputDir);
+
+  const inputDir = dirname(entryFile);
+  const file = basename(entryFile);
+  await bundle({ inputDir, outputDir, file });
+  console.log(outputDir);
+
+  return { deploymentURL: 'http://3base.com' };
 }
