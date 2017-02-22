2951defe34d723efb338104b1bea1bd542b32ce5
Manuel Vila <mvila@3base.com>
2017-02-22 15:52:53 +0900

    Use Atom 'on-save' package to automatically transpile files when they are saved

--- packages/cli/src/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-export * from './lib/forwarder';
-export * from './lib/initializer';
--- packages/website/src/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-export * from './lib/transformer';
-export * from './lib/deployer';
-export * from './lib/initializer';
-export * from './lib/s3-handler';
