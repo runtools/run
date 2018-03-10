4682abcf73731dac1caeed0f87e1e4e1ca7ace72
Manuel Vila <mvila@3base.com>
2018-03-10 09:58:51 +0900

    cli: Add macOS and Linux packers

--- cli/src/bin/index.js
+++ cli/src/bin/index.js
@@ -2,18 +2,8 @@
 
 import 'source-map-support/register';
 import {join} from 'path';
-import nodeVersion from 'node-version';
-import updateNotifier from 'update-notifier';
 import dotenv from 'dotenv';
-import {printErrorAndExit} from '@resdir/console';
 
 dotenv.config({path: join(__dirname, '..', '..', '..', '..', '.env')});
-
-if (nodeVersion.major < 6) {
-  printErrorAndExit('⚡run requires at least version 6 of Node.');
-}
-
-const pkg = require('../../../../package.json');
-updateNotifier({pkg}).notify();
 
 require('./main');
